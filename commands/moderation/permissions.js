c48c7032a9634edb9e635821ff40134908120409
David McGuigan <dvdmcguigan@gmail.com>
2021-05-20 22:49:24 +0100

    Added function to list commands

---
+++ commands/moderation/permissions.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'permissions',
+    aliases: ['perms'],
+    description: 'Prints permissions of caller or named users',
+    usage: '[people...]',
+    guildOnly: true,
+    execute(message, args) {
+
+        if (message.mentions.users.size) {
+            for (user of message.mentions.users) {
+                let i = 1;
+                const perms = message.channel.permissionsFor(user[1]).toArray();
+                message.channel.send(`${user[1].username}'s permissions:\n\t${perms.join(`\n\t`)}`);
+            }
+        }
+        else {
+            const perms = message.channel.permissionsFor(message.author).toArray();
+            message.channel.send(`Your permissions:\n\t${perms.join('\n\t')}`);
+        }        
+    }
+}
